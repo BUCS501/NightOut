fb07336a86ac76978d0dcc062f107a4f15bd34d0
Eren Budur <erenbudur33@gmail.com>
2022-11-21 19:03:50 -0500

    framelayout slight change

--- app/app/src/main/java/com/example/nightout/MainActivity.java
+++ app/app/src/main/java/com/example/nightout/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.nightout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.nightout.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -33,13 +36,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FirstFragment firstFragment = new FirstFragment();
+                SecondFragment secondFragment = new SecondFragment();
+                ThirdFragment thirdFragment = new ThirdFragment();
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+                        return true;
+                    case R.id.navigation_events:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
+                        return true;
+                    case R.id.navigation_notifications:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment).commit();
+                        return true;
+                }
+
+            }
+        });
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_events, R.id.navigation_notifications)
                 .build();
