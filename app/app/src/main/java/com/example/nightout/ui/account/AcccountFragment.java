e693cade2f85ca214edf4c1496d2ccaafd4396c9
samungercomputer <69651452+samungercomputer@users.noreply.github.com>
2022-12-11 23:56:00 -0500

    Its all done I think

--- app/app/src/main/java/com/example/nightout/ui/account/AcccountFragment.java
+++ app/app/src/main/java/com/example/nightout/ui/account/AcccountFragment.java
@@ -1,36 +1,46 @@
 package com.example.nightout.ui.account;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import com.example.nightout.BuildConfig;
 import com.example.nightout.DB;
 import com.example.nightout.Login;
 import com.example.nightout.MainActivity;
 import com.example.nightout.R;
 import com.example.nightout.SignUp;
+import com.example.nightout.ui.restaurants.Restaurant;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AcccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AcccountFragment extends Fragment {
+public class AcccountFragment extends Fragment implements PopupMenu.OnMenuItemClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -66,8 +76,10 @@ public class AcccountFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
+    //Set variables for onStart
     ConstraintLayout expandableView,expandableView2,expandableView3,expandableView4;
-    Button arrowBtn,arrowBtn2,arrowBtn3,arrowBtn4;
+    Button arrowBtn,arrowBtn2,arrowBtn3,arrowBtn4,locationperm,delete;
     CardView cardView,cardView2,cardView3,cardView4;
     DB dB;
 
@@ -78,19 +90,34 @@ public class AcccountFragment extends Fragment {
         Button btn = getView().findViewById(R.id.button2);
         dB = new DB(getActivity());
 
+        //This is the logout button, sends user back to login
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), Login.class));
             }
         });
+        // Use shared preferences to get the users location to display
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        String latitude = sharedPreferences.getString("device_latitude", null);
+        String longitude = sharedPreferences.getString("device_longitude", null);
 
-
+        //grabs the email and name to populate the fields
         TextView textemail = getView().findViewById(R.id.textView20);
         textemail.setText(SignUp.user);
         TextView textname = getView().findViewById(R.id.textView4);
         textname.setText(dB.getdataname((SignUp.user)));
 
+        //Checks to see if connected to location services if yes gives lat and long if no gives none
+        TextView location = getView().findViewById(R.id.testtest);
+        if (latitude == null || longitude == null){
+            location.setText("None");
+        }else {
+
+            location.setText(latitude + "  " + longitude);
+        }
+
+        //Sets up the expandable and cardviews
         expandableView = getView().findViewById(R.id.expandableView);
         arrowBtn = getView().findViewById(R.id.arrowBtn);
         cardView = getView().findViewById(R.id.cardView);
@@ -107,6 +134,9 @@ public class AcccountFragment extends Fragment {
         arrowBtn4 = getView().findViewById(R.id.arrowBtn4);
         cardView4 = getView().findViewById(R.id.cardView4);
 
+        locationperm = getView().findViewById(R.id.permission1);
+
+        //First tab in the account fragment
         arrowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -121,6 +151,16 @@ public class AcccountFragment extends Fragment {
                 }
             }
         });
+
+        //Button inside first tab sends you to the Android Settings
+        locationperm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        });
+
+        //Second tab inside the account Fragment
         arrowBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -135,6 +175,19 @@ public class AcccountFragment extends Fragment {
                 }
             }
         });
+
+        delete = getView().findViewById(R.id.delete);
+
+        //Button for launching the Delete account menu
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showPopup(view);
+            }
+        });
+
+        //Third tab in the account fragment, will launch the bookmarks
         arrowBtn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -149,6 +202,8 @@ public class AcccountFragment extends Fragment {
                 }
             }
         });
+
+        //fourth tab in the accounts fragment
         arrowBtn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -163,6 +218,9 @@ public class AcccountFragment extends Fragment {
                 }
             }
         });
+
+
+
     }
 
     @Override
@@ -172,7 +230,31 @@ public class AcccountFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_acccount, container, false);
     }
 
+    //Displays the popup menu for account deletion
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(getActivity(), v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.yesno);
+        popup.show();
+    }
+
+    //Deletes the users account
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                dB.deleter(SignUp.user);
+                startActivity(new Intent(getActivity(), Login.class));
+                return true;
+            case R.id.item2:
+                return true;
+            default:
+                return false;
+        }
+    }
 }
+
+
 
 
 
